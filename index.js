0c82d4b56e5b977b05e83ba4cd8973ee395f254b
yana <177691538+ianabond@users.noreply.github.com>
2026-05-04 17:37:14 -0400

    Made the password character selection checkboxes work

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const lower = [..."abcdefghijklmnopqrstuvwxyz"];
 const numbers = [..."0123456789"];
 const symbols = [..."!@#$%^&*()-_=+[]{};:'\",.<>/?\\|`~"];
 
- let allChars = [...upper, ...lower, ...numbers, ...symbols];
+let allChars = [];
 let password1 = document.getElementById("pass1");
 let password2 = document.getElementById("pass2");
 let generate = document.getElementById("generate");
+
 // console.log(allChars);
 // console.log(allChars.length);
 function getRandomIndex() {
@@ -16,8 +17,33 @@ function getRandomIndex() {
 generate.addEventListener("click", () => {
 
     let passLength = parseInt(document.querySelector('input[name="pass-len"]:checked').value);
+    let useUpper = document.getElementById("uppercase").checked;
+    let useLower = document.getElementById("lowercase").checked;
+    let useNumbers = document.getElementById("numbers").checked;
+    let useSymbols = document.getElementById("symbols").checked;
+    
     password1.textContent = "";
     password2.textContent = "";
+    allChars = [];
+
+    if(!useLower && !useUpper && !useNumbers && !useSymbols) {
+        allChars.push(...upper);
+        allChars.push(...lower);
+        allChars.push(...numbers);
+        allChars.push(...symbols);
+    }
+    if(useLower) {
+        allChars.push(...lower);
+    }
+    if(useUpper) {
+        allChars.push(...upper);
+    }
+    if(useNumbers) {
+        allChars.push(...numbers);
+    }
+    if(useSymbols) {
+        allChars.push(...symbols);
+    }
   
     for(let i = 0; i < passLength; i++) {
         password1.textContent += allChars[getRandomIndex()];
